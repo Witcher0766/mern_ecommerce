4af6382e978fb8d1daf4d6d333983b5b466e4886
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-04-30 21:50:55 +0530

    30april

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@gmail.com',
+        password: bcrypt.hashSync('12345', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        password: bcrypt.hashSync('12345', 10),
+        isAdmin: false,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        password: bcrypt.hashSync('12345', 10),
+        isAdmin: false,
+    },
+]
+
+
+export default users;
