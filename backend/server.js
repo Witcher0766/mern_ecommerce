f48ffe7fc0fbbc566d177313acff02d97e7870cd
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-05-01 21:56:46 +0530

    1May

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import connectDB from './db.js';
+import connectDB from './config/db.js';
 import products from './data/products.js'
 const port = process.env.PORT || 5000;
 
