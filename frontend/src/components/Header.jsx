0dd6ea981bb3786e90bca56ebdd2c01e0a1d6d7d
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-05-12 13:59:05 +0530

    12May

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,46 +1,76 @@
 import React from "react";
-import { Badge, Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import logo from '../assets/logo.png';
-import {LinkContainer} from 'react-router-bootstrap';
-import {useSelector} from 'react-redux';
+import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import {logout} from '../slices/authSlice';
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Header = () => {
-  
-  const {cartItems} = useSelector((state) => state.cart);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
 
-  console.log(cartItems);
+  // console.log(cartItems);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+      toast.success("Logout successfull")
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
 
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-        <LinkContainer to='/'>
-          <Navbar.Brand>
-          <img src={logo} alt="OnePiece" />OnePiece</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="OnePiece" />
+              OnePiece
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <LinkContainer to="/cart">
-              <Nav.Link >
-                <FaShoppingCart />
-                Cart
-                {
-                  cartItems.length > 0 && (
-                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
-                    {cartItems.reduce((a,c) => a+c.qty, 0)}
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
                     </Badge>
-                  )
-                }
-              </Nav.Link>
+                  )}
+                </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-              <Nav.Link >
-                <FaUser />
-                SignIn
-              </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser />
+                    SignIn
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
