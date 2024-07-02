5b77163f3ae26cd7d82907af94ec1f6362e748a5
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-07-02 16:35:51 +0530

    2july

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -57,8 +57,37 @@ const OrderScreen = () => {
   }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
 
 
-  const onApproveTest = async () => {
-
+  function onApprove(data, action) {
+    return action.order.capture().then(async function (details) {
+      try {
+        await payOrder({orderId, details});
+        refetch();
+        toast.success('Payment successful');
+      } catch (err) {
+        toast.error(err?.data?.message || err.message);
+      }
+    });
+  }
+  async function onApproveTest() {
+    await payOrder({orderId, details:{payer:{}}});
+    refetch();
+    toast.success('Payment successful');
+  }
+  function onError(err) {
+    toast.error(err.message);
+  }
+  function createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: order.totalPrice,
+          },
+        },
+      ],
+    }).then((orderId) => {
+      return orderId;
+    })
   }
 
 
@@ -163,9 +192,16 @@ const OrderScreen = () => {
                   {loadingPay && <Loader/>}
                   {isPending ? <Loader/> : (
                     <div>
-                      <Button
+                      {/* <Button
                       onClick={onApproveTest}
-                      style={{marginBottom: '10px'}}>Test Pay Order</Button>
+                      style={{marginBottom: '10px'}}>Test Pay Order</Button> */}
+                      <div>
+                        <PayPalButtons
+                        createOrder={createOrder}
+                        onApprove={onApprove}
+                        onError={onError}
+                        ></PayPalButtons>
+                      </div>
                     </div>
                   )}
                 </ListGroup.Item>
