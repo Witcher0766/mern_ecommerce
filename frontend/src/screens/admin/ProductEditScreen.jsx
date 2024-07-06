b3714d2ffde40ab4a4160b610acb4ae71758fbab
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-07-06 12:31:47 +0530

    fixes

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -1,148 +1,187 @@
 import { useState, useEffect } from "react";
-import {Link, useNavigate, useParams} from "react-router-dom";
-import {Form, Button} from 'react-bootstrap';
-import Message from '../../components/Message';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import FormContainer from "../../components/FormContainer";
-import {toast} from "react-toastify";
-import { useUpdateProductMutation, useGetProductDetailsQuery} from "../../slices/productsApiSlice";
+import { toast } from "react-toastify";
+import {
+  useUpdateProductMutation,
+  useGetProductDetailsQuery,
+  useUploadProductImageMutation,
+} from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
-    const {id: productId} = useParams();
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState(0);
-    const [image, setImage] = useState('');
-    const [brand, setBrand] = useState('');
-    const [category, setCategory] = useState('');
-    const [countInStock, setCountInStock] = useState(0);
-    const [description, setDescription] = useState('');
+  const { id: productId } = useParams();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
 
-    const {data: product, isLoading, refetch, error} = useGetProductDetailsQuery(productId);
+  const {
+    data: product,
+    isLoading,
+    refetch,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
-    const [updateProduct, {isLoading: loadingUpdate}] = useUpdateProductMutation();
-    const navigate = useNavigate();
+  const [updateProduct, { isLoading: loadingUpdate }] =
+    useUpdateProductMutation();
 
-    useEffect(() => {
-        if(product) {
-            setName(product.name);
-            setPrice(product.price);
-            setImage(product.image);
-            setBrand(product.brand);
-            setCategory(product.category);
-            setCountInStock(product.countInStock);
-            setDescription(product.description);
-        }
-    }, [product]);
+  const [uploadProductImage, { isLoading: loadingUpload }] =
+    useUploadProductImageMutation();
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        const updatedProduct = {
-            productId,
-            name,
-            price,
-            image,
-            brand,
-            category,
-            countInStock,
-            description
-        }
-        const result = await updateProduct(updatedProduct);
-        if(result.error) {
-            toast.error(result.error);
-        } else {
-            toast.success('Product updated');
-            navigate('/admin/productlist');
-        }
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (product) {
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setBrand(product.brand);
+      setCategory(product.category);
+      setCountInStock(product.countInStock);
+      setDescription(product.description);
     }
+  }, [product]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const updatedProduct = {
+      productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      countInStock,
+      description,
+    };
+    const result = await updateProduct(updatedProduct);
+    if (result.error) {
+      toast.error(result.error);
+    } else {
+      toast.success("Product updated");
+      navigate("/admin/productlist");
+    }
+  };
+
+  const uploadFileHandler = async (e) => {
+    // e.preventDefault();
+    console.log(e.target.files[0]);
+    // const formData = new FormData();
+    // formData.append('image', e.target.files[0]);
+    // try {
+    //     const res = await uploadProductImage(formData).unwrap();
+    //     toast.success(res.message);
+    //     setImage(res.image);
+    // } catch (err) {
+    //     toast.error(err?.data?.message || err.error);
+    // }
+  };
 
   return (
     <>
-        <Link to='/admin/productlist' className="btn btn-light my-3">
-            Go Back
-        </Link>
-        <FormContainer>
-            <h1>Edit Product</h1>
-            {loadingUpdate && <Loader/>}
+      <Link to="/admin/productlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
 
-            {isLoading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : 
-            (
-                <Form onSubmit={submitHandler}>
-                    <Form.Group controlId="name" className="my-2">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                    type="text"
-                    placeholder="Enter name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                    <Form.Group controlId="price" className="my-2">
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control
-                    type="number"
-                    placeholder="Enter Price"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
+            <Form.Group controlId="price" className="my-2">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                {/* image part of the from  */}
+            <Form.Group controlId="image" className="my-2">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+              type="text"
+              placeholder="Enter image url"
+              value={image}
+              onChange={(e) => setImage}
+              ></Form.Control>
+              <Form.Control
+              type="file"
+              label="Choose file"
+              onChange={uploadFileHandler}
+              >
+              </Form.Control>
+            </Form.Group>
 
-                    <Form.Group controlId="brand" className="my-2">
-                    <Form.Label>Brand</Form.Label>
-                    <Form.Control
-                    type="text"
-                    placeholder="Enter Brand"
-                    value={brand}
-                    onChange={(e) => setBrand(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
+            <Form.Group controlId="brand" className="my-2">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
+            <Form.Group controlId="category" className="my-2">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                    <Form.Group controlId="category" className="my-2">
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control
-                    type="text"
-                    placeholder="Enter category"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
+            <Form.Group controlId="countInStock" className="my-2">
+              <Form.Label>CountInStock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter countInStock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                    <Form.Group controlId="countInStock" className="my-2">
-                    <Form.Label>CountInStock</Form.Label>
-                    <Form.Control
-                    type="number"
-                    placeholder="Enter countInStock"
-                    value={countInStock}
-                    onChange={(e) => setCountInStock(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
+            <Form.Group controlId="description" className="my-2">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
 
-                    <Form.Group controlId="description" className="my-2">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                    type="text"
-                    placeholder="Enter description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    >
-                    </Form.Control>
-                    </Form.Group>
-
-                    <Button type="submit" variant="primary" className="my-2">Update</Button>
-                </Form>
-            )}
-        </FormContainer>
+            <Button type="submit" variant="primary" className="my-2">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
     </>
-  )
-}
+  );
+};
 
-export default ProductEditScreen
+export default ProductEditScreen;
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,41 +1,55 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
-export const  productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: () => ({
-                url: PRODUCTS_URL,
-            }),
-            providesTags: ['Products'],
-            keepUnusedDataFor: 5,
-        }),
-
-        getProductDetails: builder.query({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`
-            }),
-            keepUnusedDataFor: 5
-        }),
-        createProduct: builder.mutation({
-            query: () => ({
-                url: PRODUCTS_URL,
-                method: 'POST',
-            }),
-            invalidatesTags: ['Product'],
-        }),
-        updateProduct: builder.mutation({
-            query: (data) => ({
-                url: `${PRODUCTS_URL}/${data.productId}`,
-                method: 'PUT',
-                body: data,
-            }),
-            invalidatesTags: ['Products'],
-        }),
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      providesTags: ["Products"],
+      keepUnusedDataFor: 5,
     }),
+
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Products"],
+    }),
+
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
 });
 
 // invalidatesTags: ['Product'], stop the date to begin catch so that
 // we can get the fresh data
 
-export const {useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation} = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} = productsApiSlice;
